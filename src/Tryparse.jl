2761221944afba6b7f9317c886f31657e846748d
Tommy Hofmann <thofma@gmail.com>
2023-11-01 09:05:18 +0100

    add tests

--- src/Tryparse.jl
+++ src/Tryparse.jl
@@ -1,5 +1,223 @@
 module Tryparse
 
-# Write your package code here.
+struct ErrorParse <: Exception
+  msg
+end
+
+function Base.showerror(io::IO, err::ErrorParse)
+  print(io, "ParseError: ")
+  print(io, err.msg)
+end
+
+# Own little parse helper to beautify the error message
+function parse(s)
+  try
+    ex = Meta.parse(s)
+    return ex
+  catch e
+    if e isa Meta.ParseError
+      rethrow(ErrorParse("Error while parsing string \"$(s)\": $(e.msg)"))
+    else
+      rethrow(e)
+    end
+  end
+end
+
+# Parse expression for numbers
+function tryparse(::Type{T}, s::Union{SubString, String}) where {T <: Number}
+  ex = parse(s)
+  r = _tryparse(T, ex, false)
+  r === nothing && return r
+  # there is a chance, that this is an Int or some other number
+  try
+    s = T(r)
+    return s
+  catch e
+    if e isa InexactError
+      nothing
+    else
+      rethrow(e)
+    end
+  end
+end
+
+function _tryparse(::Type{T}, ex, maythrow = true) where {T <: Number}
+  if ex isa T || ex isa Number # parse also 2.0^3
+    try
+      return T(ex)
+    catch e
+      if !maythrow
+        return nothing
+      end
+      if e isa InexactError
+        rethrow(ErrorParse("Cannot convert \"$(ex)\" to type $(T)"))
+      else
+        rethrow(e)
+      end
+    end
+  else
+    !(ex isa Expr) && maythrow && throw(ErrorParse("Unexpected error. Please file a bug report"))
+    if ex.head === :call
+      if ex.args[1] === :+
+        return reduce(+, _tryparse(T, ex.args[i], maythrow) for i in 2:length(ex.args), init = zero(T))
+      elseif ex.args[1] === :-
+        return reduce(-, _tryparse(T, ex.args[i], maythrow) for i in 2:length(ex.args), init = zero(T))
+      elseif ex.args[1] === :*
+        return reduce(*, _tryparse(T, ex.args[i], maythrow) for i in 2:length(ex.args), init = one(T))
+      else
+        #for (sym, fun) in [(:^, ^), (:/, /), (://, //), (:%, %), (:div, div)]
+        for (sym, fun) in [(:^, ^), (:/, /), (://, //), (:%, %), (:div, div)]
+          if ex.args[1] === sym
+            return fun(_tryparse(T, ex.args[2], maythrow), _tryparse(T, ex.args[3], maythrow))
+          end
+        end
+        maythrow && throw(ErrorParse("Unknown call \"$(ex.args[1])\" while parsing $(T)"))
+      end
+    end
+  end
+  maythrow && throw(ErrorParse("Unknown syntax \"$(ex)\" while parsing $(T)"))
+  return nothing
+end
+
+# Vector
+function tryparse(::Type{Vector{T}}, s::Union{SubString, String}) where {T}
+  ex = parse(s)
+  r = _tryparse(Vector{T}, ex, false)
+  # there is a chance, that this is an Int
+  return r
+end
+
+function _tryparse(::Type{Vector{T}}, ex, maythrow = true) where {T}
+  if ex isa Expr
+    if ex.head === :vect
+      return T[_tryparse(T, a, maythrow) for a in ex.args]
+    elseif ex.head === :comprehension
+      maythrow && throw(ErrorParse("List comprehension not supported."))
+    end
+  end
+  maythrow && throw(ErrorParse("Unkown syntax for vector construction. Please use \"[...]\"."))
+  return nothing
+end
+
+# Tuple
+function tryparse(S::Type{<:Tuple}, s::Union{SubString, String}) 
+  ex = parse(s)
+  r = _tryparse(S, ex, false)
+  # there is a chance, that this is an Int
+  return r
+end
+
+function _tryparse(S::Type{<:Tuple}, ex, maythrow = true) 
+  fT = fieldtypes(S)
+  if ex isa Expr
+    if ex.head === :tuple
+      return ntuple(i -> _tryparse(fT[i], ex.args[i], maythrow), length(ex.args))
+    end
+  end
+  maythrow && throw(ErrorParse("Unkown syntax for tuple construction. Please use \"(...)\"."))
+  return nothing
+end
+
+# Matrix
+function tryparse(::Type{Matrix{T}}, s::Union{SubString, String}) where {T}
+  ex = parse(s)
+  r = _tryparse(Matrix{T}, ex, false)
+  # there is a chance, that this is an Int
+  return r
+end
+
+function _tryparse(::Type{Matrix{T}}, ex, maythrow = true) where {T}
+  if ex isa Expr
+    if ex.head === :vcat
+      nr = length(ex.args)
+      nc = length(ex.args[1].args)
+      if any(a -> length(a.args) != nc, ex.args)
+        maythrow && throw(ErrorParse("Unequal number of columns"))
+        return nothing
+      end
+      M = Matrix{T}(undef, nr, nc)
+      for (i, a) in enumerate(ex.args)
+        a.head !== :row && maythrow && throw(ErrorParse("Unexpected error. Please file a bug report"))
+        for (j, v) in enumerate(a.args)
+          M[i, j] = _tryparse(T, v, maythrow)
+        end
+      end
+      return M
+    end
+  end
+  maythrow && throw(ErrorParse("Unkown syntax for matrix construction. Please use \"[...;...]\"."))
+  return nothing
+end
+
+# UnitRange
+
+function tryparse(::Type{UnitRange{T}}, s::Union{SubString, String}) where {T}
+  ex = parse(s)
+  r = _tryparse(UnitRange{T}, ex, false)
+  return r
+end
+
+function _tryparse(::Type{UnitRange{T}}, ex, maythrow = true) where {T}
+  if ex isa Expr
+    if ex.head === :call
+      if ex.args[1] === :(:)
+        if length(ex.args) == 3
+          a = _tryparse(T, ex.args[2], maythrow)
+          a === nothing && return a
+          b = _tryparse(T, ex.args[3], maythrow)
+          b === nothing && return b
+          return a:b
+        end
+      end
+    end
+  end
+  maythrow && throw(ErrorParse("Unkown syntax \"$(ex)\" for unit range construction. Please use \"...:...\"."))
+  return nothing
+end
+
+# Steprange
+function tryparse(::Type{StepRange{T}}, s::Union{SubString, String}) where {T}
+  ex = parse(s)
+  r = _tryparse(StepRange{T}, ex, false)
+  return r
+end
+
+function _tryparse(::Type{StepRange{T}}, ex, maythrow = true) where {T}
+  if ex isa Expr
+    if ex.head === :call
+      if ex.args[1] === :(:)
+        if length(ex.args) == 4
+          a = _tryparse(T, ex.args[2], maythrow)
+          a === nothing && return a
+          b = _tryparse(T, ex.args[3], maythrow)
+          b === nothing && return b
+          c = _tryparse(T, ex.args[4], maythrow)
+          c === nothing && return c
+          return a:b:c
+        end
+      end
+    end
+  end
+  maythrow && throw(ErrorParse("Unkown syntax \"$(ex)\" for step range construction. Please use \"...:...:...\"."))
+  return nothing
+end
+
+# parse
+function parse(T::Type, s)
+  r = _tryparse(T, parse(s), true)
+  if T <: Number
+    try
+      return T(r)
+    catch e
+      if e isa InexactError
+        rethrow(ErrorParse("Cannot convert \"$(s)\" to type $(T)"))
+      else
+        rethrow(e)
+      end
+    end
+  else
+    return r
+  end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,165 @@
 using Tryparse
 using Test
 
+tryparse = Tryparse.tryparse
+parse = Tryparse.parse
+ErrorParse = Tryparse.ErrorParse
+
 @testset "Tryparse.jl" begin
-    # Write your tests here.
+  # Int's
+  for T in [Int32, Int, Int128, BigInt]
+    for (val, str) in [(-1, "-1"), (1, "1"), (2, "1 + 1"), (-7, "1 - 2^3"), (5, "div(10, 2)"), (12, "2 * 3 * 2")]
+      x = tryparse(T, str)
+      @test x isa T
+      @test x == val
+      y = parse(T, str)
+      @test y isa T
+      @test y == val
+    end
+  end
+
+  x = tryparse(BigInt, "10^50")
+  @test x isa BigInt && x == big(10)^50
+
+  @test tryparse(Int, "1//2") === nothing
+  @test_throws ErrorParse parse(Int, "1//2")
+  @test tryparse(Int, "1 + ") === nothing
+  @test_throws ErrorParse parse(Int, "1 + ")
+  @test tryparse(Int, "1 + (2 + 3") === nothing
+  @test_throws ErrorParse parse(Int, "1 + (2 + 3")
+  @test tryparse(Int, "sin(2)") === nothing
+  @test_throws ErrorParse parse(Int, "sin(2)")
+
+  # Float's
+  for T in [Float32, Float64, BigFloat]
+    for (val, str) in [(-1.0, "-1.0"), (1, "1"), (2, "1 + 1"), (-7, "1 - 2^3"), (5, "div(10, 2)"), (1, "1.0/1.0"), (12, "2.0 * 3.0 * 2.0")]
+      x = tryparse(T, str)
+      @test x isa T
+      @test x == val
+      y = parse(T, str)
+      @test y isa T
+      @test y == val
+    end
+  end
+
+  # Rational
+  for T in [Rational{Int32}, Rational{Int}, Rational{Int128}, Rational{BigInt}]
+    for (val, str) in [(1, "1"), (2, "1 + 1"), (-7, "1 - 2^3"), (5, "10//2"), (12, "2 * 3 * 2"), (3//4, "(3//2)//(2//1)")]
+      x = tryparse(T, str)
+      @test x isa T
+      @test x == val
+      y = parse(T, str)
+      @test y isa T
+      @test y == val
+    end
+  end
+
+  # Vector
+  x = tryparse(Vector{Int}, "[1]")
+  @test x isa Vector{Int} && x == [1]
+  x = parse(Vector{Int}, "[1]")
+  @test x isa Vector{Int} && x == [1]
+
+  x = tryparse(Vector{Int}, "[1,]")
+  @test x isa Vector{Int} && x == [1]
+  x = parse(Vector{Int}, "[1,]")
+  @test x isa Vector{Int} && x == [1]
+
+  x = tryparse(Vector{Int}, "[1,2^10]")
+  @test x isa Vector{Int} && x == [1, 2^10]
+  x = parse(Vector{Int}, "[1,2^10]")
+  @test x isa Vector{Int} && x == [1, 2^10]
+
+  x = tryparse(Vector{BigInt}, "[1,2^100]")
+  @test x isa Vector{BigInt} && x == [1, big(2)^100]
+
+  @test tryparse(Vector{Int}, "1") === nothing
+  @test_throws ErrorParse parse(Vector{Int}, "1")
+  @test tryparse(Vector{Int}, "[1,2") === nothing
+  @test_throws ErrorParse parse(Vector{Int}, "[1,2")
+
+  # Matrix
+  x = tryparse(Matrix{Int}, "[1 2;]")
+  @test x isa Matrix{Int} && x == [1 2;]
+  x = parse(Matrix{Int}, "[1 2;]")
+  @test x isa Matrix{Int} && x == [1 2;]
+
+  x = tryparse(Matrix{Int}, "[1 2; 3 4; 5 6]")
+  @test x isa Matrix{Int} && x == [1 2; 3 4; 5 6]
+  x = parse(Matrix{Int}, "[1 2; 3 4; 5 6]")
+  @test x isa Matrix{Int} && x == [1 2; 3 4; 5 6]
+
+  x = tryparse(Matrix{Int}, "[1 2; 3+3 4; 5 6]")
+  @test x isa Matrix{Int} && x == [1 2; 6 4; 5 6]
+  x = parse(Matrix{Int}, "[1 2; 3+3 4; 5 6]")
+  @test x isa Matrix{Int} && x == [1 2; 6 4; 5 6]
+
+  x = tryparse(Matrix{Int}, "[1 2; 3+3 4; 5 6]")
+  @test x isa Matrix{Int} && x == [1 2; 6 4; 5 6]
+  x = parse(Matrix{Int}, "[1 2; 3+3 4; 5 6]")
+  @test x isa Matrix{Int} && x == [1 2; 6 4; 5 6]
+
+  x = tryparse(Matrix{BigInt}, "[1 2; 2^100 4; 5 6]")
+  @test x isa Matrix{BigInt} && x == [1 2; big(2)^100 4; 5 6]
+  x = parse(Matrix{BigInt}, "[1 2; 2^100 4; 5 6]")
+  @test x isa Matrix{BigInt} && x == [1 2; big(2)^100 4; 5 6]
+
+  # UnitRange
+  x = tryparse(UnitRange{Int}, "1:0")
+  @test x isa UnitRange{Int} && x == 1:0
+  x = parse(UnitRange{Int}, "1:0")
+  @test x isa UnitRange{Int} && x == 1:0
+
+  x = tryparse(UnitRange{Int}, "-1:2^3")
+  @test x isa UnitRange{Int} && x == -1:2^3
+  x = parse(UnitRange{Int}, "-1:2^3")
+  @test x isa UnitRange{Int} && x == -1:2^3
+
+#  x = tryparse(UnitRange{Float64}, "1.0:0.0")
+#  @test x isa UnitRange{Float64} && x == 1.0:0.0
+#  x = parse(UnitRange{Float64}, "1.0:0.0")
+#  @test x isa UnitRange{Float64} && x == 1.0:0.0
+#
+#  x = tryparse(UnitRange{Float64}, "-1.0:2.0^3")
+#  @test x isa UnitRange{Float64} && x == -1.0:2.0^3
+#  x = parse(UnitRange{Float64}, "-1.0:2.0^3")
+#  @test x isa UnitRange{Float64} && x == -1.0:2.0^3
+
+  @test tryparse(UnitRange{Int}, "1:0:0") === nothing
+  @test_throws ErrorParse parse(UnitRange{Int}, "1:0:0")
+  @test tryparse(UnitRange{Int}, "[1,2]") === nothing
+  @test_throws ErrorParse parse(UnitRange{Int}, "[1,2]")
+  @test tryparse(UnitRange{Int}, "1.2:3.0") === nothing
+  @test_throws ErrorParse parse(UnitRange{Int}, "1.2:3.0")
+  @test tryparse(UnitRange{Int}, "1:3:2") === nothing
+  @test_throws ErrorParse parse(UnitRange{Int}, "1:3:2")
+
+  # StepRange
+  
+  x = tryparse(StepRange{Int}, "1:2:5")
+  @test x isa StepRange{Int} && x == 1:2:5
+  x = parse(StepRange{Int}, "1:2:5")
+  @test x isa StepRange{Int} && x == 1:2:5
+
+  x = tryparse(StepRange{Int}, "-1:2^3:100")
+  @test x isa StepRange{Int} && x == -1:2^3:100
+  x = parse(StepRange{Int}, "-1:2^3:100")
+  @test x isa StepRange{Int} && x == -1:2^3:100
+
+#  x = tryparse(UnitRange{Float64}, "1.0:0.0")
+#  @test x isa UnitRange{Float64} && x == 1.0:0.0
+#  x = parse(UnitRange{Float64}, "1.0:0.0")
+#  @test x isa UnitRange{Float64} && x == 1.0:0.0
+#
+#  x = tryparse(UnitRange{Float64}, "-1.0:2.0^3")
+#  @test x isa UnitRange{Float64} && x == -1.0:2.0^3
+#  x = parse(UnitRange{Float64}, "-1.0:2.0^3")
+#  @test x isa UnitRange{Float64} && x == -1.0:2.0^3
+
+  @test tryparse(StepRange{Int}, "1:0") === nothing
+  @test_throws ErrorParse parse(StepRange{Int}, "1:0")
+  @test tryparse(StepRange{Int}, "[1,2]") === nothing
+  @test_throws ErrorParse parse(StepRange{Int}, "[1,2]")
+  @test tryparse(StepRange{Int}, "1.2:3.0:5.0") === nothing
+  @test_throws ErrorParse parse(StepRange{Int}, "1.2:3.0:5.0")
 end
